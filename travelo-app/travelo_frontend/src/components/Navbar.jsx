d5710bb8c6f80771c939253b9e7449a1b8ce0d60
meryam27 <meryamelqemary1@gmail.com>
2026-04-14 21:08:32 +0100

    changing navbar 2

--- travelo-app/travelo_frontend/src/components/Navbar.jsx
+++ travelo-app/travelo_frontend/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar() {
       <ul>
         <li>
           <div className="logo">
-            <span className="bitra">Travelo updated by dev2</span>
+            <span className="bitra">Travelo updated by dev2 number2</span>
           </div>
         </li>
         <li>
